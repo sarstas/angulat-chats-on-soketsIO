998f0f90897df51a288f73c52a0e3d16b9603606
sar.stas <sar.stanislav13@gmail.com>
2021-11-18 02:16:15 +0300

    init

--- appIO/README.md
+++ appIO/README.md
@@ -1,4 +1,4 @@
-# AngulatChatsOnSoketsIO
+# AppIO
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.0.2.
 
--- appIO/karma.conf.js
+++ appIO/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angulat-chats-on-soketsIO'),
+      dir: require('path').join(__dirname, './coverage/appIO'),
       subdir: '.',
       reporters: [
         { type: 'html' },
