3797959f0af3339f2cd456ee2a6e74eba2862a91
sar.stas <sar.stanislav13@gmail.com>
2021-11-19 13:52:40 +0300

    added validation form

--- socketio-angular/src/app/send-form/send-form.component.ts
+++ socketio-angular/src/app/send-form/send-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder} from "@angular/forms";
+import {FormBuilder, Validators} from "@angular/forms";
 
 import {SocketioService} from "../socketio.service";
 
@@ -11,7 +11,7 @@ import {SocketioService} from "../socketio.service";
 export class SendFormComponent implements OnInit {
 
   checkoutForm = this.formBuilder.group({
-    message: ''
+    message: ['', Validators.required]
   });
 
   constructor(
